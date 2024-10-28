93d0b97d90051b862db53e7bbed1ddfdc6f33942
Ferran Maura <ferranjosep.maura@upf.edu>
2024-10-28 12:01:41 +0100

    tracing using samply

--- run.sh
+++ run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 
-simTime=1000.0
-mean_length=1000.0
+simTime=100
+
 k_queue=100
+mean_length=1000.0
+
+
 rate_bps_src=2000.0
 rate_bps_queue=20000.0
 
 
-rm out_log.ans
+distance=1.0
 
-# script -q -c "cargo run --example mm1k_sim $simTime $mean_length $k_queue $rate_bps_src $rate_bps_queue" out_log.ans
 
-cargo run --example mm1k_sim $simTime $mean_length $k_queue $rate_bps_src $rate_bps_queue # for no DBG output file
+# rm out_log.ans
+
+# script -q -c "cargo run --example mm1k_sim $simTime $mean_length $k_queue $rate_bps_src $rate_bps_queue $distance" out_log.ans
+
+cargo run --example mm1k_sim $simTime $mean_length $k_queue $rate_bps_src $rate_bps_queue $distance # for no DBG output file
+
+
+# trace with samply:
+# samply record cargo run --example mm1k_sim $simTime $mean_length $k_queue $rate_bps_src $rate_bps_queue $distance # for Tracing syscalls
